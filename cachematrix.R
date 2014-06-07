14b58e8c7261d53516c4c69fcdbdf17355ed1d7b
Terence Lam <terenceltl@gmail.com>
2014-06-07 13:27:24 +0800

    First sumbit, tested with matrices

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,43 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## This makeCacheMatrix function creates a
+## special matrix object that can cache
+## its own  matirx inversion, by taking
+## advantage of the  <<- operator
 
+makeCacheMatrix <- function(invertible = matrix()) {
+  cachedinversion <- NULL
+  set <- function(y) {
+  invertible <<- y
+  cachedinversion <<- NULL
+  }
+  get <- function() invertible
+  setinversion <- function(inversion) cachedinversion <<- inversion
+  getinversion <- function() cachedinversion
+  list(set = set, get = get,
+       setinversion = setinversion,
+       getinversion = getinversion)  
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## This cacheSolve function computes the inverse of
+## the special "matrix" returned by makeCacheMatrix above.
+## If the inverse has already been calculated (and the
+## matrix has not changed), then the cachesolve should
+## retrieve the inverse from the cache.
+
+cacheSolve <- function(invertible, ...) {
+## Return a matrix that is the inverse of 'x'
+  inversion <- invertible$getinversion()
+  if(!is.null(inversion)) {
+    message("getting cached data")
+    return(inversion)
+  }
+  data <- invertible$get()
+  inversion <- solve(data, ...)
+  invertible$setinversion(inversion)
+  inversion  
 }
